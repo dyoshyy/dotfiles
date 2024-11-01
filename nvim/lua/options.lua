5fc8b1eb6e5973b70934c3d49bdfc1034d5a9774
dyoshyy <d.yoshhyy@gmail.com>
2024-11-01 23:51:08 +0900

    fix: 折り返しを有効に

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -21,8 +21,6 @@ vim.api.nvim_win_set_option(0, 'number', true)
 vim.api.nvim_win_set_option(0, 'cursorline', true)
 -- 標識のためのスペースを最左列に設ける
 vim.api.nvim_win_set_option(0, 'signcolumn', 'yes:1')
--- テキストの折り返しを無効化
-vim.api.nvim_win_set_option(0, 'wrap', false)
 -- 非表示文字の可視化
 vim.api.nvim_win_set_option(0, 'list', true)
 -- 指定したカラム列を強調
